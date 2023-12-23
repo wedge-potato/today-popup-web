2c51ba4086d543760a49a6efc21440b4f60cd8b9
da-in <da-in@wavve.com>
2023-12-23 22:41:49 +0900

    feat: top bar

---
+++ src/components/top-bar/TopBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-between bg-gray-200">
+    <button v-if="!isHome" @click="onClickBack">뒤로가기</button>
+    <h1 v-if="isHome">서비스명 + 로고</h1>
+    <button v-if="!isSetting" @click="onClickSetting">설정</button>
+  </div>
+</template>
+<script setup lang="ts">
+
+import {useRouter} from "vue-router";
+import {computed} from "vue";
+
+const router = useRouter()
+const currentPath = computed(() => router.currentRoute.value.path)
+
+const isHome = computed(() => currentPath.value === '/')
+const isSetting = computed(() => currentPath.value === '/setting')
+
+const onClickBack = () => {
+  router.back()
+}
+
+const onClickSetting = () => {
+  router.push('/setting')
+}
+</script>
