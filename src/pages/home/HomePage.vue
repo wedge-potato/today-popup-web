0d38eacdd3f96b5daf44b9135d996326143e0ced
da-in <da-in@wavve.com>
2023-12-24 00:39:02 +0900

    feat: sort

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,5 +1,9 @@
 <template>
   <top-bar />
+  <div class='flex justify-between'>
+    <span>지역</span>
+    <button @click='changeSort'>{{ sortTextRef }}</button>
+  </div>
   <popup-list :popup-list='data' @click='onClick' />
 </template>
 <script setup lang='ts'>
@@ -8,10 +12,18 @@ import TopBar from '../../components/top-bar/TopBar.vue'
 import { getPopupList } from '../../requests/getPopupList.ts'
 import PopupList from '../../components/popup/PopupList.vue'
 import { useRouter } from 'vue-router'
+import { computed, ref } from 'vue'
 
 const router = useRouter()
 
 const data = getPopupList()
+
+const sortRef = ref<'new' | 'end'>('new')
+const sortTextRef = computed(() => sortRef.value === 'new' ? '최신 오픈순' : '종료 날짜순')
+
+const changeSort = () => {
+  sortRef.value = sortRef.value === 'new' ? 'end' : 'new'
+}
 
 const onClick = (id: number) => {
   router.push(`/detail/${id}`)
